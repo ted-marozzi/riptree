f538c8643ab8485cf17b44dbfba90c67cbb04c90
Ted Marozzi <edwardmarozzi@gmail.com>
2023-04-04 20:34:54 +1000

    Implemented peekable iter and combined loop

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,28 @@
-use std::fs::{self, DirEntry};
+use std::{
+    fs::{self},
+    path::Path,
+};
 
-pub fn run() {
-    let path = "./";
-
+pub fn run(path: &Path) {
+    println!("{}", path.display());
     print_dir_recursively(path, "");
 }
 
-fn print_dir_recursively(path: &str, line_prefix: &str) {
-    let dir_contents = fs::read_dir(path).unwrap();
-    let mut dirs: Vec<DirEntry> = vec![];
-    let mut files: Vec<DirEntry> = vec![];
-
-    for dir_entry_result in dir_contents {
-        let dir_entry = dir_entry_result.unwrap();
-        let file_type = dir_entry.file_type().unwrap();
-        if file_type.is_dir() {
-            dirs.push(dir_entry);
-        } else if file_type.is_file() {
-            files.push(dir_entry);
-        }
-    }
-
-    let files_count = files.len();
-    let dirs_count = dirs.len();
-    for (index, file) in files.iter().enumerate() {
-        if index == files_count - 1 && dirs_count == 0 {
-            println!("{}└── {}", line_prefix, file.file_name().to_str().unwrap());
-        } else {
-            println!("{}├── {}", line_prefix, file.file_name().to_str().unwrap());
-        }
-    }
-
-    for (index, dir) in dirs.iter().enumerate() {
-        let file_name = dir.file_name();
+fn print_dir_recursively(path: &Path, line_prefix: &str) {
+    let mut dir_entries = fs::read_dir(path).unwrap().map(|d| d.unwrap()).peekable();
+    while let Some(entry) = dir_entries.next() {
+        let file_name = entry.file_name();
         let file_name_str = file_name.to_str().unwrap();
 
-        let new_line_prefix = if index == dirs_count - 1 {
+        let new_line_prefix = if dir_entries.peek().is_none() {
             println!("{}└── {}", line_prefix, file_name_str);
             format!("{line_prefix}    ")
         } else {
             println!("{}├── {}", line_prefix, file_name_str);
             format!("{line_prefix}│   ")
         };
-
-        print_dir_recursively(dir.path().to_str().unwrap(), &new_line_prefix)
+        if entry.file_type().unwrap().is_dir() {
+            print_dir_recursively(&entry.path(), &new_line_prefix)
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::{env, path::Path};
+
 fn main() {
-    rip_tree::run()
+    let args: Vec<String> = env::args().collect();
+
+    let path = if args.len() == 0 || args.len() == 1 {
+        Path::new(".")
+    } else if args.len() == 2 {
+        Path::new(args.get(1).unwrap())
+    } else {
+        panic!("riptree received more than one argument: {:?}.\nriptree only accepts one argument for the Path to tree which defaults to current working directory.", args)
+    };
+
+    riptree::run(path)
 }
