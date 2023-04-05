b02507f3eeda0e5b832a8c8904fde378e49e44fd
Ted Marozzi <tedmarozzi@canva.com>
2023-04-05 13:01:32 +1000

    Minor Refactor

--- src/lib.rs
+++ src/lib.rs
@@ -6,17 +6,16 @@ use std::{
 pub fn run(path: &Path) {
     if path.exists() {
         println!("{}", path.display());
+        print_dir_recursively(path, "");
     } else {
         println!("The path provided doesn't exist.\nPath: {}", path.display());
-        return;
     }
-    print_dir_recursively(path, "");
 }
 
 fn print_dir_recursively(path: &Path, line_prefix: &str) {
     let mut dir_entries = match fs::read_dir(path) {
+        Ok(entries) => entries.peekable(),
         Err(_) => return,
-        Ok(entries) => entries.peekable()
     };
 
     while let Some(entry_result) = dir_entries.next() {
@@ -29,23 +28,23 @@ fn print_dir_recursively(path: &Path, line_prefix: &str) {
             continue;
         };
 
-        let new_line_prefix = if dir_entries.peek().is_none() {
-            println!("{}└── {}", line_prefix, file_name);
-            format!("{line_prefix}    ")
-        } else {
+        let new_line_prefix = if dir_entries.peek().is_some() {
             println!("{}├── {}", line_prefix, file_name);
             format!("{line_prefix}│   ")
+        } else {
+            println!("{}└── {}", line_prefix, file_name);
+            format!("{line_prefix}    ")
         };
 
         match entry.file_type() {
-            Err(_) => {
-                continue;
-            },
             Ok(file_type) => {
                 if file_type.is_dir()  {
                     print_dir_recursively(&entry.path(), &new_line_prefix);
                 }
-            }
+            },
+            Err(_) => {
+                continue;
+            },
         }
     }
     
