b3f0cf76058e3d80e62262804c05b70b055145e7
Ted Marozzi <edwardmarozzi@gmail.com>
2023-04-05 21:41:26 +1000

    print dirs

--- src/lib.rs
+++ src/lib.rs
@@ -6,23 +6,31 @@ use std::{
 pub fn run(path: &Path) {
     if path.exists() {
         println!("{}", path.display());
-        print_dir_recursively(path, "");
+        let (dirs_count, files_count) = print_tree(path);
+        println!("\n{dirs_count} directories, {files_count} files");
     } else {
         println!("The path provided doesn't exist.\nPath: {}", path.display());
     }
 }
 
-fn print_dir_recursively(path: &Path, line_prefix: &str) {
+fn print_tree(path: &Path) -> (u32, u32) {
+    let (dirs_count, files_count) = print_dir_recursively(path, "");
+    return (dirs_count + 1, files_count);
+}
+
+fn print_dir_recursively(path: &Path, line_prefix: &str) -> (u32, u32) {
+    let mut dirs_count = 0;
+    let mut files_count = 0;
     let mut dir_entries = match fs::read_dir(path) {
         Ok(entries) => entries.peekable(),
-        Err(_) => return,
+        Err(_) => return (dirs_count, files_count),
     };
 
     while let Some(entry_result) = dir_entries.next() {
         let Ok(entry) = entry_result else {
             continue;
         };
-    
+
         let file_name_os_string = entry.file_name();
         let Some(file_name) = file_name_os_string.to_str() else {
             continue;
@@ -38,14 +46,20 @@ fn print_dir_recursively(path: &Path, line_prefix: &str) {
 
         match entry.file_type() {
             Ok(file_type) => {
-                if file_type.is_dir()  {
-                    print_dir_recursively(&entry.path(), &new_line_prefix);
+                if file_type.is_dir() {
+                    dirs_count += 1;
+                    let (child_dirs_count, child_files_count) =
+                        print_dir_recursively(&entry.path(), &new_line_prefix);
+                    dirs_count += child_dirs_count;
+                    files_count += child_files_count;
+                } else if file_type.is_file() {
+                    files_count += 1;
                 }
-            },
+            }
             Err(_) => {
                 continue;
-            },
+            }
         }
     }
-    
+    return (dirs_count, files_count);
 }
